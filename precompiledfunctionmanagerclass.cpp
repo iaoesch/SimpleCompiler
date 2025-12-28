a135602acc1ccdd265e092936d1e5743f75aace0
osi1 <osi1@bfh.ch>
2025-12-28 11:12:13 +0100

    started templatemagie

--- precompiledfunctionmanagerclass.cpp
+++ precompiledfunctionmanagerclass.cpp
@@ -47,77 +47,97 @@ void PrecompiledFunctionManagerClass::RegisterFunction(std::string Name, Functio
 
 }
 
+template <class T>
+struct Convertor {
+    static VariableTypeDescriptorClass MakeDescriptor() {return VariableTypeDescriptorClass(TypeDescriptorClass::Type::Undefined);}
+};
 
-#if 0
+template <>
+struct Convertor<int64_t> {
+    static VariableTypeDescriptorClass MakeDescriptor() {return VariableTypeDescriptorClass(TypeDescriptorClass::Type::Integer);}
+};
 
-Anonymeousfunctiondefinition:
-                               "function" {
-    drv.Currentfunction.BeginFunctionDefinition(@1);
-    FunctionsDefinitonsPending.push_back({});
-    FunctionDefinitionInfoType &CurrentFunctionInfo = this->FunctionsDefinitonsPending.back();
-    CurrentFunctionInfo.VariableHoldingCurrentFunction = Variables.GetVariableReference(Name);
-    if (CurrentFunctionInfo.VariableHoldingCurrentFunction != nullptr) {
-        throw(yy::parser::syntax_error(l, "function allready defined"));
+template <>
+struct Convertor<double> {
+    static VariableTypeDescriptorClass MakeDescriptor() {return VariableTypeDescriptorClass(TypeDescriptorClass::Type::Float);}
+};
+
+template <>
+struct Convertor<std::string> {
+    static VariableTypeDescriptorClass MakeDescriptor() {return VariableTypeDescriptorClass(TypeDescriptorClass::Type::String);}
+};
+
+template <>
+struct Convertor<Variables::ListClass> {
+    static VariableTypeDescriptorClass MakeDescriptor() {return VariableTypeDescriptorClass(TypeDescriptorClass::Type::List);}
+};
+
+template <>
+struct Convertor<Variables::ArrayClass> {
+    static VariableTypeDescriptorClass MakeDescriptor(Variables::ArrayClass &a) {return a.GetTypeDescriptor();}
+};
+
+typedef std::variant<std::monostate,
+                     int64_t,
+                     double,
+                     std::string,
+                     Variables::StackClass,
+                     Variables::ListClass,
+                     Variables::ArrayClass,
+                     Variables::MapClass,
+                     TypeDescriptorClass,
+                     std::shared_ptr<ExpressionClass>,
+                     std::shared_ptr<Variables::FunctionDefinitionBaseClass>,
+                     std::shared_ptr<Variables::VariableContentClass>> dataType;
+
+
+template <class ...P>
+class ParameterListBuilder {
+public:
+    static std::vector<VariableTypeDescriptorClass> BuildParameterTypeList()
+    {
+        std::vector<VariableTypeDescriptorClass> v{Convertor<P>::MakeDescriptor()...};
+        return v;
     }
-    CurrentFunctionInfo.VariableHoldingCurrentFunction = Variables.CreateVariable(Name, VariableTypeDescriptorClass(TypeDescriptorClass::Type::Function), 0.0);
-    CurrentFunctionInfo.CurrentFunction = std::make_shared<Variables::FunctionDefinitionClass>(Name, l);
-    CurrentFunctionInfo.VariableHoldingCurrentFunction->SetValue(Variables::VariableContentClass(CurrentFunctionInfo.CurrentFunction));
-    CurrentFunctionInfo.Name = Name;
-    //auto &i = typeid(CurrentFunction);
-    return CurrentFunctionInfo.CurrentFunction;
+};
 
-}
-functionBodydefinition
+void test ()
 {
-    $$ = $3;
+    std::vector<VariableTypeDescriptorClass> v = ParameterListBuilder<int64_t, float, float>::BuildParameterTypeList();
 }
-;
 
-
-functiondefinition:
-                     "function" "identifier"
+template <class R, class ...P>
+std::vector<VariableTypeDescriptorClass> BuildParameterTypeList(R (*f)(P...))
 {
-    drv.Currentfunction.BeginFunctionDefinition($2, @2);
+    return ParameterListBuilder<P...>::BuildParameterTypeList();
 }
-functionBodydefinition
+
+int f(double d, int i, double d2);
+
+void test2 ()
 {
-    $$ = $4;
+    std::vector<VariableTypeDescriptorClass> v = BuildParameterTypeList(f);
 }
-;
 
-functionBodydefinition:
-{
-    /*FktDefContainer tmp;*/
-    auto ptr = drv.Currentfunction.GetReference();
-    /*$<FktDefContainer>$ = tmp;*/
-    drv.Variables.StartLocal(ptr);
-    drv.Variables.CreateNewContext(drv.Currentfunction.GetName()+"Params");
-}
-returntype.opt
-    "(" argumentlist ")"    {
-    drv.Variables.CreateNewContext(drv.Currentfunction.GetName());
-    //auto ReturnValue = drv.Variables.CreateVariable($<std::string>0, *$2, 0.0);
-    drv.Currentfunction.SetReturnType(std::move($2));
-    drv.Currentfunction.Set($4, @4);
-}
-statements
-    "endfunction" {
-    /**$<FktDefContainer>3 = Variables::FunctionDefinitionClass($5, $8);*/
-    /*$$ = $<FktDefContainer>3.ptr;*/
-    auto StorageTemplate = drv.Variables.EndLocal();
-    //drv.Currentfunction.Define($4, $7, std::move(StorageTemplate), @1+@5);
-    drv.Currentfunction.Set($7, @7);
-    drv.Currentfunction.Set(std::move(StorageTemplate), @1+@5);
-    $$ = drv.Currentfunction.Get(@7); drv.Variables.LeaveContext(2);
-}
-| error "endfunction" {$$ = std::make_shared<Variables::FunctionDefinitionClass>(Variables::FunctionDefinitionClass::MakeEmpty());}
-;
+template <class First>
+class ParameterListBuilder2 {
+    void BuildParameterTypeList(std::vector<VariableTypeDescriptorClass> &v)
+    {
+        ParameterListBuilder<P...> B;
+        Convertor<First> C;
+        v.push_back(C.MakeDescriptor());
+        B.BuildParameterTypeList(v);
+    }
+};
 
-returntype.opt:
-                 %empty                       {$$ = std::make_unique<VariableTypeDescriptorClass>(TypeDescriptorClass::Type::Undefined);}
-| "returning" typedefinition {$$ = std::move($2);}
+template <class First, class ...P>
+class ParameterListBuilder2 {
+    void BuildParameterTypeList(std::vector<VariableTypeDescriptorClass> &v)
+    {
+        ParameterListBuilder<P...> B;
+        Convertor<First> C;
+        v.push_back(C.MakeDescriptor());
+        B.BuildParameterTypeList(v);
+    }
+};
 
-argumentlist:
-               "identifier"           {$$ = std::vector<std::shared_ptr<VariableClass>>(); auto var = drv.Variables.CreateVariable($1, VariableTypeDescriptorClass(TypeDescriptorClass::Type::Dynamic), 0.0); $$.push_back(var);}
-| argumentlist "," "identifier" {auto var = drv.Variables.CreateVariable($3, VariableTypeDescriptorClass(TypeDescriptorClass::Type::Dynamic), 0.0); $1.push_back(var); $$ = $1; };
-#endif
